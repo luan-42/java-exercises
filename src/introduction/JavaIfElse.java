ab5d2e02593e2daf29924f09211d74db7b2210c2
Luan Ferreira <178341285+luan-42@users.noreply.github.com>
2024-10-07 18:09:35 -0300

    :sparkles: feat: Add JavaIfElse

---
+++ src/introduction/JavaIfElse.java
@@ -0,0 +1,19 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class JavaIfElse {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main( String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        scanner.close();
+
+        if (N % 2 == 0 && (N >= 2 && N <= 5 || N > 20)) {
+            System.out.println("Not Weird");
+        } else {
+            System.out.println("Weird");
+        }
+    }
+}
